1acc67f2211270cac64873e57d644c9ade9df122
obirikan <obirikan020@gmail.com>
2022-03-03 04:44:50 +0000

    all

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import Chat from './Chat'
+import io from 'socket.io-client'
+import { useState } from 'react';
+import { Center, Button, Container, Stack, Input, Box, Heading } from '@chakra-ui/react'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+
+
+const socket=io.connect('https://chitchart.herokuapp.com')
+
+
+function App() {
+  const [name,setname]=useState('')
+  const [room,setroom]=useState('')
+  const [show,setshow]=useState(true)
+
+
+  const joinRoom=async ()=>{
+     if(room !== "" && name !== ""){
+      setshow(!show)
+      
+     await socket.emit("join_room",room)
+     }
+  }
+
+
+  return (
+    <div className='conatiner'>
+      
+        {show?(
+    <>
+       <Center  h='400px'  color='black'>
+       <Container mt={200}>
+         <Box
+          d='flex'
+          justifyContent='center'
+          alignItems='center'
+         ><Heading  >Rooms</Heading></Box>
+         <Stack spacing={4} marginTop='2'>
+                <Input placeholder='name' size='md' width='auto' onChange={(e)=>setname(e.target.value)} />
+                <Input placeholder='room' size='md' width='auto' onChange={(e)=>setroom(e.target.value)}/>
+                <Button rightIcon={<ArrowForwardIcon />} width='auto' onClick={joinRoom} colorScheme='blue'   size='sm'>
+                  Join room
+               </Button>
+          </Stack>
+       </Container>
+       </Center>
+    </>
+       ):<Chat name={name} socket={socket} room={room}/>}
+    </div>
+  );
+}
+
+export default App;
